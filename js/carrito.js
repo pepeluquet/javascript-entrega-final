46fb9323c3d45ffe11abd531a56c22d1dd0ec50f
Christian Luquet <pepeluquet@hotmail.com>
2025-05-04 17:51:52 -0300

    aplicando bootstrap al carrito

--- js/carrito.js
+++ js/carrito.js
@@ -7,13 +7,17 @@ carritoStorage = JSON.parse(carritoStorage)
 function renderCarrito(carritoItems) {
     carritoItems.forEach(producto => {
         const carrito = document.createElement("div")
-            carrito.innerHTML = `   <div class="card">
-                                        <div class="card-img-overlay">
-                                            <h4 class="card-title">${producto.nombre}</h4>
-                                            <h4 class="card-title">$ ${producto.precio}</h4>
-                                            <button class="btn btn-light" id="${producto.id}">Eliminar</button>
+            carrito.innerHTML = `   <article class="col">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${producto.nombre}</h5>
+                                                <h5 class="card-title">$ ${producto.precio}</h5>
+                                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                                    <button class="btn btn-light" id="${producto.id}">Eliminar</button>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>`
+                                    </article>`
         carritoContainer.appendChild(carrito)
     })
     console.log(carritoProductos)
