c3b05b6207727b82471efc70b3b060651c787377
Christian Luquet <pepeluquet@hotmail.com>
2025-05-26 16:48:19 -0300

    fetch

--- js/mainCarrito.js
+++ js/mainCarrito.js
@@ -1,26 +1,4 @@
-const productos = [
-    {
-        id: 1,
-        nombre: "Estiramientos dinámicos para atletas.",
-        descripcion: "Este libro se centra en los estiramientos dinámicos que son útiles para el rendimiento deportivo.",
-        precio: 12000.00,
-        imagen: "./assets/foto2.webp"
-    },
-    {
-        id: 2,
-        nombre: "Clases de Yoga y Stretching para hacer en sus casas",
-        descripcion: "8 videos con clases destinadas para realizar en la comodidad de sus casas y en el tiempo que deseen.",
-        precio: 20000.00,
-        imagen: "./assets/foto3.webp"
-    },
-    {
-        id: 3,
-        nombre: "Suscripción",
-        descripcion: "Suscripción mensual a clases en vivo.",
-        precio: 18000.00,
-        imagen: "./assets/foto4.webp"
-    },
-]
+let productos = []
 
 let carritoProductos = JSON.parse(localStorage.getItem("carritoProductos")) || []
 
@@ -143,14 +121,20 @@ if (vaciarCarritoButton) {
     vaciarCarritoButton.addEventListener("click", vaciarCarrito)
 }
 
-if (document.getElementById("productos-container")) {
-    renderProductos()
-    actualizarCuentaCarrito()
-}
-if (document.getElementById("carrito-section")) {
-    renderCarrito()
-    actualizarCuentaCarrito()
-}
+
+fetch("../db/data.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data
+        if (document.getElementById("productos-container")) {
+            renderProductos()
+            actualizarCuentaCarrito()
+        }
+        if (document.getElementById("carrito-section")) {
+            renderCarrito()
+            actualizarCuentaCarrito()
+        }
+    })
 
 function actualizarCuentaCarrito() {
     const cuentaCarrito = document.getElementById("cuenta-carrito");
